bd6b8978f2ad6b4e2f0da96192de824f7a23f7fd
Wesal Ismail <wsalasmayl363@gmail.com>
2026-08-22 01:32:43 +0300

    integerate Login backend

--- src/authentication/Login.tsx
+++ src/authentication/Login.tsx
@@ -1,16 +1,50 @@
-import { LogIn, Eye, EyeClosed } from "lucide-react"
+import { LogIn, Eye, EyeClosed, CircleAlert } from "lucide-react"
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import { useAuth } from "../context/useAuth"
+import { login } from "../api/auth"
+import type { LogUser } from "../types/auth"
 
 function Login() {
+  const { setToken } = useAuth()
+  const navigate = useNavigate()
+
   const [showPassword, setShowPassword] = useState(false)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [message, setMessage] = useState("")
+
+  const showMessage = (message: string) => {
+    setMessage(message)
+
+    setTimeout(() => {
+      setMessage("")
+    }, 3500)
+  }
+
+  const handleLogin = async () => {
+    const user: LogUser = {
+      password: password,
+      username: username
+    }
+    try {
+      const response = await login(user)
+
+      setToken(response.token)
+      navigate("/home")
+    } catch (error) {
+      showMessage("Incorrect username or password")
+      console.log(error)
+    }
+  }
+
   return (
     <div className="flex w-full items-center justify-center px-6 py-12 lg:w-1/2">
       <div className="w-full max-w-md">
         {/* Logo */}
 
         {/* Heading */}
-        <div className="mb-8">
+        <div className="mb-8 relative">
           <h1 className="font-heading text-3xl font-bold text-primary-font sm:text-4xl">
             Welcome back
           </h1>
@@ -18,6 +52,13 @@ function Login() {
           <p className="mt-2 font-body text-sm text-primary-font/60">
             Log in to continue managing your projects.
           </p>
+
+          {message && (
+            <div className="absolute text-xs left-0 top-full mt-2 flex w-fit items-center gap-1 rounded-xl bg-redT/15 px-3 py-0.5">
+              <CircleAlert className="h-4 w-4 text-redT" />
+              <p className="font-body text-sm text-redT">{message}</p>
+            </div>
+          )}
         </div>
 
         {/* Form */}
@@ -33,6 +74,8 @@ function Login() {
 
             <input
               id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               type="text"
               placeholder="Enter your username"
               className="w-full rounded-2xl border border-primary/15 bg-white py-3.5 px-4 font-body text-sm text-primary-font outline-none transition-all duration-300 placeholder:text-primary-font/40 focus:border-primary focus:ring-2 focus:ring-primary/10"
@@ -49,7 +92,10 @@ function Login() {
                 Password
               </label>
 
-              <Link to="/forgot-password" className="font-body text-xs font-medium text-primary transition-colors duration-300 hover:text-primary-font">
+              <Link
+                to="/forgot-password"
+                className="font-body text-xs font-medium text-primary transition-colors duration-300 hover:text-primary-font"
+              >
                 Forgot password?
               </Link>
             </div>
@@ -57,6 +103,8 @@ function Login() {
             <div className="relative">
               <input
                 id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 type={showPassword ? "text" : "password"}
                 minLength={6}
                 placeholder="Enter your password"
@@ -81,13 +129,13 @@ function Login() {
           </div>
 
           {/* Login */}
-          <Link
-            to="/home"
+          <button
+            onClick={handleLogin}
             className="mt-3 flex w-full items-center gap-2 rounded-2xl border border-primary/15 bg-primary px-4 py-3 font-body text-white shadow-sm transition-all text-center duration-300 hover:-translate-y-0.5 hover:shadow-md hover:bg-secondary"
           >
             <LogIn className="h-5 w-5 mr-2" />
             Log In
-          </Link>
+          </button>
         </div>
 
         {/* Register */}
