6f1b108649137829ede0223b62af8192fab28681
Wesal Ismail <wsalasmayl363@gmail.com>
2026-08-21 04:46:50 +0300

    activate Explore mode

---
+++ src/api/auth.ts
@@ -0,0 +1,21 @@
+const BASE_URL = import.meta.env.VITE_BASE_URL
+import axios from "axios"
+import type { LogUser, NewUser } from "../types/auth"
+
+export async function register(newUser: NewUser) {
+  const response = await axios.post(`${BASE_URL}/auth/register`, newUser)
+
+  return response.data
+}
+
+export async function login(logUser:LogUser) {
+  const response = await axios.post(`${BASE_URL}/auth/login`, logUser)
+
+  return response.data
+}
+
+export async function explore() {
+  const response = await axios.post(`${BASE_URL}/auth/explore`)
+
+  return response.data
+}
