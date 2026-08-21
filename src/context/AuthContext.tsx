207ea6355f777fc901ec053f6b3d6334c938c6d8
Wesal Ismail <wsalasmayl363@gmail.com>
2026-08-21 05:22:03 +0300

    save token to local storage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,7 +2,7 @@ import { createContext } from "react"
 
 type AuthContextType = {
   token: string
-  setToken: React.Dispatch<React.SetStateAction<string>>
+  setToken: (token:string) => void
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined)
--- src/context/AuthContextProvider.tsx
+++ src/context/AuthContextProvider.tsx
@@ -2,10 +2,17 @@ import { AuthContext } from "./AuthContext"
 import { useState } from "react"
 
 function AuthContextProvider({ children }: { children: React.ReactNode }) {
-  const [token, setToken] = useState('')
+  const [token, setTokenState] = useState(
+    () => localStorage.getItem("token") ?? ""
+  )
+
+  const setToken = (newToken: string) => {
+    localStorage.setItem("token", newToken)
+    setTokenState(newToken)
+  }
 
   return (
-    <AuthContext.Provider value={{token, setToken}}>
+    <AuthContext.Provider value={{ token, setToken }}>
       {children}
     </AuthContext.Provider>
   )
