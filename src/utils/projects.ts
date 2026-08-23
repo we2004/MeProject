a36afe6bb8f936a91ce23c070b5d81d68169846e
Wesal Ismail <wsalasmayl363@gmail.com>
2026-08-23 07:07:01 +0300

    refactor utilities

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -1,11 +1,8 @@
 import { type Task } from "../types/tasks"
 import {
-  type Project,
   type ProjectApiResponse,
   type ProjectStatusFilter
 } from "../types/projects"
-import dayjs from "dayjs"
-import { type SortOrder } from "../types/common"
 
 export function calculateProgress(
   projectId: number,
@@ -36,16 +33,6 @@ export function getOngoingProjects(
   return onGoionProjects
 }
 
-export function sortProjectsByDueDate(
-  projects: Project[],
-  sortOrder: SortOrder = "asc"
-) {
-  return [...projects].sort((a, b) =>
-    sortOrder === "asc"
-      ? dayjs(a.dueDate).diff(dayjs(b.dueDate))
-      : dayjs(b.dueDate).diff(dayjs(a.dueDate))
-  )
-}
 
 export function getProjectFilter(
   value: string | undefined | null
@@ -59,74 +46,4 @@ export function getProjectFilter(
     default:
       return "all"
   }
-}
-
-export function getDisplayedProjectStat(
-  cancelled: boolean | undefined,
-  progress: number,
-  dueDate: string | undefined
-) {
-  let displayedStat: ProjectStatusFilter
-  if (cancelled) displayedStat = "cancelled"
-  else if (progress === 100) displayedStat = "completed"
-  else if (dayjs(dueDate).isBefore(dayjs(), "day")) displayedStat = "overdue"
-  else displayedStat = "active"
-
-  return displayedStat
-}
-
-export function getOverdueProjets(projects: ProjectApiResponse[], tasks: Task[]) {
-  const activeProjects = projects.filter(
-    (project) =>
-      calculateProgress(project.id, tasks) !== 100 && !project.cancelled
-  )
-  return activeProjects.filter((project) =>
-    dayjs(project.dueDate).isBefore(dayjs(), "day")
-  )
-}
-
-export function getActiveProjects(projects: ProjectApiResponse[], tasks: Task[]) {
-  const activeProjects = projects.filter(
-    (project) =>
-      calculateProgress(project.id, tasks) !== 100 && !project.cancelled
-  )
-
-  return activeProjects.filter(
-    (project) => !dayjs(project.dueDate).isBefore(dayjs(), "day")
-  )
-}
-
-export function filterProjectsByStat(
-  projects: ProjectApiResponse[],
-  filter: ProjectStatusFilter,
-  tasks: Task[]
-) {
-  switch (filter) {
-    case "cancelled":
-      return projects.filter((project) => project.cancelled)
-    case "active":
-      return getActiveProjects(projects, tasks)
-    case "overdue":
-      return getOverdueProjets(projects, tasks)
-
-    case "completed":
-      return projects.filter(
-        (project) =>
-          calculateProgress(project.id, tasks) === 100 && !project.cancelled
-      )
-
-    default:
-      return projects
-  }
-}
-
-export function getPageProjects(
-  projects: ProjectApiResponse[],
-  filter: ProjectStatusFilter,
-  order: SortOrder,
-  tasks: Task[]
-) {
-  const filteredProjects = filterProjectsByStat(projects, filter, tasks)
-
-  return sortProjectsByDueDate(filteredProjects, order)
 }
--- src/utils/tasks.ts
+++ src/utils/tasks.ts
@@ -4,7 +4,6 @@ import {
   type TaskPriorityFilter
 } from "../types/tasks"
 import dayjs from "dayjs"
-import { type SortOrder } from "../types/common"
 
 export function getOngoingTasks(tasks: Task[]) {
   const onGoionProjects = tasks.filter((task) => task.status != "completed")
@@ -12,16 +11,6 @@ export function getOngoingTasks(tasks: Task[]) {
   return onGoionProjects
 }
 
-export function sortTasksByDueDate(
-  tasks: Task[],
-  sortOrder: SortOrder = "asc"
-) {
-  return [...tasks].sort((a, b) =>
-    sortOrder === "asc"
-      ? dayjs(a.dueDate).diff(dayjs(b.dueDate))
-      : dayjs(b.dueDate).diff(dayjs(a.dueDate))
-  )
-}
 
 export function getOverdueTasks(tasks: Task[]) {
   return tasks.filter(
@@ -64,50 +53,4 @@ export function getTaskPriority(value: string | null): TaskPriorityFilter {
   }
 }
 
-export function filterTasksByStat(tasks: Task[], filter: TaskStatusFilter) {
-  switch (filter) {
-    case "open":
-      return getOpenTasks(tasks)
-    case "completed":
-      return getCompletedTasks(tasks)
 
-    case "overdue":
-      return getOverdueTasks(tasks)
-    default:
-      return tasks
-  }
-}
-
-export function filterTasksByPriority(
-  tasks: Task[],
-  priority: TaskPriorityFilter
-) {
-  if (priority === "all") return tasks
-  return tasks.filter((task) => task.priority === priority)
-}
-
-export function getTasksByProjectId(
-  tasks: Task[],
-  projectId: string | null | undefined,
-  order: SortOrder = "asc"
-) {
-  return sortTasksByDueDate(
-    tasks.filter((task) => task.projectId === Number(projectId)),
-    order
-  )
-}
-
-export function getPageTasks(
-  tasks: Task[],
-  filter: TaskStatusFilter,
-  order: SortOrder,
-  priority: TaskPriorityFilter,
-  projectId: string | null | undefined
-) {
-  const realTasks = projectId ? getTasksByProjectId(tasks, projectId) : tasks
-  const filteredTasks = filterTasksByPriority(
-    filterTasksByStat(realTasks, filter),
-    priority
-  )
-  return sortTasksByDueDate(filteredTasks, order)
-}
