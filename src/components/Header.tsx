3ee775aa391ab5db769edb936b7d432ce6b6b399
Wesal Ismail <wsalasmayl363@gmail.com>
2026-08-25 04:17:21 +0300

    update: remove search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Search, UserRound } from "lucide-react"
+import { UserRound } from "lucide-react"
 import logo from "../assets/logo2.png"
 import { useAuth } from "../context/useAuth"
 import { getUser } from "../api/auth"
@@ -25,7 +25,7 @@ function Header() {
 
   return (
     <header className="border border-primary/15 px-4 py-3 shadow-sm backdrop-blur-sm sticky top-0 z-40">
-      <div className="mx-auto flex items-center gap-3 ">
+      <div className="mx-auto flex items-center justify-between gap-3 ">
         <div className="flex items-center gap-1 ">
           <img
             src={logo}
@@ -42,16 +42,7 @@ function Header() {
           </div>
         </div>
 
-        <div className="flex-1 min-w-0">
-          <label className="group flex items-center gap-3 rounded-2xl border border-primary/15 bg-white/80 px-4 py-3 shadow-sm transition-all duration-300 hover:-translate-y-0.5 hover:border-primary/60 hover:shadow-md">
-            <Search className="h-4 w-4 text-primary-font/60 transition-colors duration-300 group-hover:text-primary" />
-            <input
-              type="text"
-              placeholder="Search projects"
-              className="w-full border-none bg-transparent font-body text-sm text-primary-font outline-none placeholder:text-primary-font/50"
-            />
-          </label>
-        </div>
+       
 
         <div className="flex items-center self-end sm:self-auto">
           <Link to='/settings' className="flex items-center gap-3 rounded-2xl border border-primary/15 bg-white/80 px-3 py-2 shadow-sm transition-all duration-300 hover:-translate-y-0.5 hover:shadow-md hover:border-primary/60 ">
